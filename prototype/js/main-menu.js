e5266e0ed67133ec658c03a72c1c1238221e09ec
SeanJM <seanjmacisaac@gmail.com>
2012-10-11 12:27:01 -0400

    File menu has better colors and spacing and sub menus for mini menus appear in the correct place as well as the regular menu

--- prototype/js/main-menu.js
+++ prototype/js/main-menu.js
@@ -9,26 +9,33 @@ $(function(){
   }
 
   function miniMenu() {
-    var i   = 0,
-        ul  = $('#main-menu .menu.first ul'),
-        li  = $('#main-menu .menu.first ul li'),
-        firstLayer = $('#main-menu .menu.first').children('li');
+    var i           = 0,
+        ul          = $('#main-menu .menu.first ul'),
+        li          = $('#main-menu .menu.first ul li'),
+        firstLayer  = $('#main-menu > ul > li');
     
-    li.each(function(){
-      i++;
-      
-      var container = $(this).parent().find('.mini-menu-container'),
-          clss      = $(this).attr('class'),
-          marker    = $(this).find('.marker'),
-          miniMenu  = $('<div class="miniMenu" index="' + i + '"><div class="innerShadow"></div></div>').addClass(clss);
-      
-      if (marker.size()) { 
-        var markerColor = marker.css('background-color'); 
-        miniMenu.css('background-color',markerColor);
-      }
+    ul.each(function(){
+      var li = $(this).find('li');
+      li.each(function(){
+        i++;
+        
+        var container = $(this).parent().find('.mini-menu-container'),
+            clss      = $(this).attr('class'),
+            marker    = $(this).find('.marker'),
+            miniMenu  = $('<div class="miniMenu" index="' + i + '"><div class="innerShadow"></div></div>').addClass(clss),
+            disclosure= $('<span class="disclosure" />');
+        
+        if (marker.size()) { 
+          var markerColor = marker.css('background-color'); 
+          miniMenu.css('background-color',markerColor);
+        }
 
-      $(this).attr('index',i);
-      container.append(miniMenu);
+        $(this).attr('index',i);
+        if ($(this).find('ul').size()) {
+          $(this).append(disclosure);
+        }
+        container.append(miniMenu);
+      });
       
     });
 
@@ -39,69 +46,98 @@ $(function(){
       parent.find('.miniMenu').removeClass('hover');
       parent.find('li').removeClass('hover');
       parent.find('.miniMenu[index="' + index + '"]').addClass('hover');
-    
+      
     });
 
     /* Activate Sub menu */
     
-    li.bind('mouseover',function(){
+    li.on('mouseover',function(){
       if ($(this).children('ul').size()) { 
         var offset          = parseInt($(this).parent().css('width')) - 4,
             vertical_offset = $(this).outerHeight() * -1,
             subMenu         = $(this).children('ul'),
             width           = subMenu.find('li').width();
         
-        subMenu
-          .addClass('visible')
-          .css('left',offset)
-          .css('top',vertical_offset);
+        if (!subMenu.hasClass('visible')) {
+          subMenu
+            .addClass('visible')
+            .css('left',offset)
+            .css('top',vertical_offset);
+        } 
       }
     });
 
     li.on('miniover',function(){
 
+      $(this)
+        .parent().find('ul.visible').removeClass('visible');
+
       if ($(this).children('ul').size()) { 
         var index             = $(this).attr('index'),
+            subMenu           = $(this).children('ul'),
             miniMenu          = $(this).parent().find('.miniMenu[index="' + index + '"]');
+            miniMenuPos       = miniMenu.position()['top'];
             horizontal_offset = '-8px',
             vertical_offset   = miniMenu.outerHeight() + parseInt(miniMenu.css('margin-bottom')),
-            vertical_position = ((index * vertical_offset) + $(this).outerHeight()) * -1;
+            vertical_position = ($(this).position()['top'] * -1) + (miniMenuPos - miniMenu.outerHeight());
         
-        $(this)
-          .children('ul')
+        subMenu
           .addClass('visible')
           .css('left',horizontal_offset)
           .css('top',vertical_position);
+
       }
+
+
     });
 
     li.on('mouseleave',function(){
+      var index     = $(this).attr('index'),
+          miniMenu  = $(this).parent().find('.miniMenu[index="' + index + '"]');
+
+      miniMenu.removeClass('hover');
+      
       if ($(this).children('ul').size()) { 
         $(this)
           .children('ul')
           .removeClass('visible')
           .trigger('minileave');
       }
-      console.log($(this));
     });
 
     $('.miniMenu').on('mouseover',function(){
       var index   = $(this).attr('index'),
           parent  = $(this).parents('ul');
+      
       parent.find('.miniMenu').removeClass('hover');
       parent.find('li').removeClass('hover');
       
-      parent
-        .find('li[index="' + index + '"]')
+      parent.find('li[index="' + index + '"]')
         .addClass('hover')
         .trigger('miniover');
     });
     
-    firstLayer.on('click',function(){
+    firstLayer.on('click',function(e){
       $(this).children('ul').toggleClass('visible');
+    });
+
+    $('html').on('click',function(){
+      if (firstLayer.find('.visible').size() > 0) {
+        /*firstLayer.find('.visible').removeClass('visible');*/
+      }
     });
   }
 
+  $('#main-menu ul.menu.first .container > li').on('mousedown',function(event){
+    /* Right Click Options */
+    if (event.which == 3) { 
+      console.log(event.which);
+      event.preventDefault();
+    }
+  });
+  $('#main-menu ul.menu.first .container > li').on('contextmenu',function(event){
+    return false;
+  });
   positionMenu();
   $('#main-menu .ai').click(function(){
     $('#main-menu').addClass('visible');
