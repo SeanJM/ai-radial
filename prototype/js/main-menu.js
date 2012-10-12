923139984d44ecd5adb615e68d6d56780424912b
SeanJM <seanjmacisaac@gmail.com>
2012-10-12 12:27:46 -0400

    Right click menu for configuring window appearance and templated the file dropdown

--- prototype/js/main-menu.js
+++ prototype/js/main-menu.js
@@ -26,8 +26,8 @@ $(function(){
             disclosure= $('<span class="disclosure" />');
         
         if (marker.size()) { 
-          var markerColor = marker.css('background-color'); 
-          miniMenu.css('background-color',markerColor);
+          var markerColor = marker.attr('class').replace('marker','').trim(); 
+          miniMenu.attr('class','miniMenu ' + markerColor);
         }
 
         $(this).attr('index',i);
@@ -38,6 +38,7 @@ $(function(){
       });
       
     });
+
 
     li.on('hover',function(){
       var index   = $(this).attr('index'),
@@ -77,15 +78,20 @@ $(function(){
             subMenu           = $(this).children('ul'),
             miniMenu          = $(this).parent().find('.miniMenu[index="' + index + '"]');
             miniMenuPos       = miniMenu.position()['top'];
-            horizontal_offset = '-8px',
             vertical_offset   = miniMenu.outerHeight() + parseInt(miniMenu.css('margin-bottom')),
-            vertical_position = ($(this).position()['top'] * -1) + (miniMenuPos - miniMenu.outerHeight());
+            vertical_position = ($(this).position()['top'] * -1) + (miniMenuPos - miniMenu.outerHeight()),
+            horizontal_offset = '-8px';
         
+        if ($('#main-menu').hasClass('right-handed')) {
+          horizontal_offset = parseInt($(this).parents('.menu-root').css('padding-right')) * -1;
+        }
+        if ($('#main-menu').hasClass('left-handed')) {
+          horizontal_offset = parseInt($(this).parents('.menu-root').css('padding-right')) - 5;
+        }
         subMenu
           .addClass('visible')
           .css('left',horizontal_offset)
           .css('top',vertical_position);
-
       }
 
 
@@ -127,21 +133,98 @@ $(function(){
       }
     });
   }
+  function load_template(data,callback) {
+    var cache = $('<div />'),
+        templateAddress = './templates/' + data['template-file'] + '.html' + ' ' + data['template'],
+        newCache;
+    if ($(data['template']).size)
+    cache.load(templateAddress,function() {
+      newCache = cache.replaceWith(cache.contents());
+      newCache.appendTo(data['parent']);
+      if (callback) {
+        callback(newCache);
+      }
+    });
+  }
 
-  $('#main-menu ul.menu.first .container > li').on('mousedown',function(event){
-    /* Right Click Options */
-    if (event.which == 3) { 
-      console.log(event.which);
-      event.preventDefault();
+  load_template(
+    {
+      'template-file':'menu',
+      'template':'#file-menu',
+      'parent':'#main-menu .menu.first li.file'
+    },function() {
+
+      miniMenu(); 
+      bindConfigureMenu();
+
+    });
+  
+  function menuBind(data) { 
+    var el    = data['element'],
+        index = el.attr('index'),
+        mini  = el.parents('#main-menu').find('.miniMenu[index="' + index + '"]'),
+        menu  = $('#configure-menu');
+
+    el.addClass('select');
+
+    menu.on('contextmenu',function(){ return false; });
+    menu.css('top',data['mouse'].pageY-10).css('left',data['mouse'].pageX-10);
+    menu.find('.mark').off('click');
+    menu.find('.mark').on('click',function(){
+      var color = $(this).attr('class').replace('mark','').replace('color','').trim();
+      el.find('.marker').attr('class','marker ' + color);
+      mini.attr('class','miniMenu ' + color);
+    });
+    
+    function hideMenu() {
+      if (menu.is(':visible')) { menu.hide(); }
+      el.removeClass('select');
+    } 
+    menu.on('click',function (){ hideMenu(); });
+    menu.on('mouseleave',function(){ hideMenu(); });
+
+    if (menu.is(':hidden')) { menu.show(); }
+
+    $('#hide-entry').off('click');
+    $('#hide-entry').on('click',function(){
+      el.addClass('hidden');
+      mini.addClass('hidden');
+    });
+
+    $('#show-hidden').off('click');
+    $('#show-hidden').on('click',function(){
+      el.parent().find('.hidden').removeClass('hidden');
+      mini.parent().find('.hidden').removeClass('hidden');
+    });
+
+  }
+
+  function bindConfigureMenu() {
+    $('#main-menu ul.menu.first .container > li').on('mousedown',function(event){
+      /* Right Click Options */
+      if (event.which == 3) {
+        configureMenu({'element': $(this),'mouse':event});
+      }
+    });
+  }
+
+  function configureMenu(data) {
+    var el      = data['element'],
+        cache   = $('<div />'),
+        menu    = '#configure-menu';
+    
+
+    /* Check to see if it exists */
+    if ($(menu).size() < 1) { 
+      load_template({'template-file':'menu','template':menu,'parent':'body'},function() { menuBind(data); }); 
     }
-  });
-  $('#main-menu ul.menu.first .container > li').on('contextmenu',function(event){
-    return false;
-  });
-  positionMenu();
+
+    menuBind(data);
+  }
+
+  $('#main-menu *').on('contextmenu',function(event){ return false; });
   $('#main-menu .ai').click(function(){
     $('#main-menu').addClass('visible');
   });
-
-  miniMenu();
-});  
+  positionMenu();
+}); 
